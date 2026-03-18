3bd6894b9f399dc62682ebabe9c0627b428fb5b0
sagarboyal <sboyal12@gmail.com>
2026-03-18 20:43:42 +0530

    feat: add global exception handler and custom exceptions

---
+++ backend/src/main/java/com/sagarboyal/aiassessment/common/exception/custom/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.sagarboyal.aiassessment.common.exception.custom;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
